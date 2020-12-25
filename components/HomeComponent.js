d7c8798d6c2e10aa7b304fe8f650343864b98464
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-12-25 07:01:56 -0800

    add cards to home page

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,16 +1,52 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
+import { PARTNERS } from "../shared/partners";
+import { PROMOTIONS } from "../shared/promotions";
 
-export class HomeComponent extends Component {
+function RenderItem({ item }) {
+  if (item) {
+    return (
+      <Card
+        featuredTitle={item.name}
+        image={require("../shared/images/react-lake.jpg")}
+      >
+        <Text style={{ margin: 10 }}>{item.description}</Text>
+      </Card>
+    );
+  }
+  return <View />;
+}
+class HomeComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      promotions: PROMOTIONS,
+      partners: PARTNERS,
+    };
+  }
+
   static navigationOptions = {
     title: "Home",
   };
 
   render() {
     return (
-      <View>
-        <Text>Home Component</Text>
-      </View>
+      <ScrollView>
+        <RenderItem
+          item={this.state.campsites.filter((campsite) => campsite.featured)[0]}
+        />
+        <RenderItem
+          item={
+            this.state.promotions.filter((promotion) => promotion.featured)[0]
+          }
+        />
+        <RenderItem
+          item={this.state.partners.filter((partner) => partner.featured)[0]}
+        />
+      </ScrollView>
     );
   }
 }
