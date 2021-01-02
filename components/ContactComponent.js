870cb4df196c383a8079625bbdf173fce567b283
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-01-02 11:11:53 -0800

    render contact information on contanct screen

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Text } from "react-native";
+import { Card } from "react-native-elements";
 
 
 class Contact extends Component {
@@ -10,7 +11,15 @@ class Contact extends Component {
 
   render() {
     return (
-    <ScrollView></ScrollView>
+    <ScrollView>
+      <Card title="Contact Information" wrapperStyle={{margin: 20}}>
+        <Text>1 Nucamp Way</Text>
+        <Text>Seattle, WA 98001</Text>
+        <Text>USA</Text>
+        <Text style={{marginTop: 10}}>Phone: 1-206-555-1234</Text>
+        <Text>Email: campsites@nucamp.com</Text>
+      </Card>
+    </ScrollView>
     );
   }
 }
