4161bedc53dcc3868685406a8945973b7a0d1ae7
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-01-06 19:51:15 -0800

    add custom header to side drawer

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,11 +4,12 @@ import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
-import { View, Platform, StyleSheet } from "react-native";
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
-import { createDrawerNavigator } from "react-navigation-drawer";
+import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 import { Icon } from "react-native-elements";
+import SafeAreaView from 'react-native-safe-area-view';
 
 const ContactNavigatior = createStackNavigator(
   {
@@ -115,6 +116,29 @@ const HomeNavigator = createStackNavigator(
   }
 );
 
+const CustomDrawerContentComponent = props => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{top: 'always', horizontal: 'never'}}
+    >
+      <View style={styles.drawerHeader}>
+        <View style={{flex: 1}}>
+          <Image
+            source={require('../shared/images/logo.png')}
+            style={styles.drawerImage}
+            />
+        </View>
+        <View style={{flex: 2}}>
+          <Text style={styles.drawerHeaderText}>NuCamp</Text>
+        </View>
+      </View>
+      <DrawerItems { ...props } />
+    </SafeAreaView>
+  </ScrollView>
+)
+
+
 const MainNavigator = createDrawerNavigator(
   {
     Home: {
@@ -166,6 +190,7 @@ const MainNavigator = createDrawerNavigator(
   },
   {
     drawerBackgroundColor: "#CEC8FF",
+    contentComponent: CustomDrawerContentComponent
   }
 );
 
@@ -188,6 +213,27 @@ class Main extends Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  drawerHeader: {
+    backgroundColor: '#5637DD',
+    height: 140,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row"
+  },
+  drawerHeaderText: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  drawerImage: {
+    margin: 10,
+    height: 60,
+    width: 60
+  },
   stackIcon: {
     marginLeft: 10,
     color: "#fff",
