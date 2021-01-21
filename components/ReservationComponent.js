f4e9d91f62aac7017a4405158b9311e8eef484f5
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-01-21 08:49:51 -0800

    add Notification alert when submiting reservation request

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
 
 class Reservation extends Component {
   constructor(props) {
@@ -44,9 +45,15 @@ class Reservation extends Component {
           style: "cancel",
           onPress: () => this.resetForm(),
         },
+
         {
           text: "OK",
-          onPress: () => this.resetForm(),
+          onPress: () => {
+            this.presentLocalNotification(
+              this.state.date.toLocaleDateString("en-US")
+            );
+            this.resetForm();
+          },
         },
       ],
       { cancelable: false }
@@ -62,6 +69,34 @@ class Reservation extends Component {
       showModal: false,
     });
   }
+
+  async presentLocalNotification(date) {
+    console.log("IN PRESENTLOCALNOTIFICATION::::", date);
+    function sendNotification() {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+        }),
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Your Campsite Reservation Search",
+          body: `Search for ${date} requested`,
+        },
+        trigger: null,
+      });
+    }
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
+  }
+
+
 
   render() {
     return (
