d2f620b0701c63824cc4ae66e9c2e45654734e99
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-01-23 10:53:41 -0800

    reformat images to make them smaller and PNG format as users take a picture

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { View, StyleSheet, ScrollView, Image } from "react-native";
 import { Input, Button, Icon, CheckBox } from "react-native-elements";
+import * as ImageManipulator from "expo-image-manipulator";
 import * as SecureStore from "expo-secure-store";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
@@ -162,9 +163,19 @@ class RegisterTab extends Component {
 
       if (!capturedImage.cancelled) {
         console.log(capturedImage);
-        this.setState({ imageUrl: capturedImage.uri });
+        this.processImage(capturedImage.uri);
       }
     }
+  };
+
+  processImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(
+      imgUri,
+      [{ resize: { width: 400 } }],
+      { format: ImageManipulator.SaveFormat.PNG }
+    );
+    this.setState({ imageUri: processedImage.uri });
+    console.log("PROCESSED IMAGE YO >>> ", processedImage);
   };
 
   handleRegister() {
